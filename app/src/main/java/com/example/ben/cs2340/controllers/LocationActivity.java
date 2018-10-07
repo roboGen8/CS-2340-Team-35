3763657d76a6666fae9503261c838e2a2d9cf28f
Ben <benrutherford@me.com>
2018-10-07 13:33:02 -0400

    work on getting locations screen set up

--- app/src/main/java/com/example/ben/cs2340/controllers/LocationActivity.java
+++ app/src/main/java/com/example/ben/cs2340/controllers/LocationActivity.java
@@ -4,12 +4,22 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.example.ben.cs2340.R;
+import com.example.ben.cs2340.model.Location;
+import com.example.ben.cs2340.model.LocationAdapter;
+import com.example.ben.cs2340.model.LocationManager;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class LocationActivity extends AppCompatActivity {
+
+    ArrayList<Location> locations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +28,24 @@ public class LocationActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        RecyclerView rvLocations = (RecyclerView) findViewById(R.id.rvLocations);
+        try {
+            locations = LocationManager.parseData();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        LocationAdapter adapter = new LocationAdapter(locations);
+        rvLocations.setAdapter(adapter);
+        rvLocations.setLayoutManager(new LinearLayoutManager(this));
     }
 
 }
--- app/src/main/java/com/example/ben/cs2340/model/LocationManager.java
+++ app/src/main/java/com/example/ben/cs2340/model/LocationManager.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class LocationManager {
 
@@ -14,19 +15,20 @@ public class LocationManager {
     }
 
 
-    private ArrayList<Location> _locations = new ArrayList<>();
+//    private ArrayList<Location> _locations = new ArrayList<>();
+//
+//
+//    private boolean addLocation(String name, float latitude, float longitude, String address, String city, String state, String zip, String type, String phone, String website) {
+//       Location newLocation = new Location(name, latitude, longitude, address, city, state, zip, type, phone, website);
+//       return _locations.add(newLocation);
+//    }
+//
+//    public ArrayList<Location> getLocations() {
+//        return _locations;
+//    }
 
-
-    private boolean addLocation(String name, float latitude, float longitude, String address, String city, String state, String zip, String type, String phone, String website) {
-       Location newLocation = new Location(name, latitude, longitude, address, city, state, zip, type, phone, website);
-       return _locations.add(newLocation);
-    }
-
-    public ArrayList<Location> getLocations() {
-        return _locations;
-    }
-
-    public void parseData() throws IOException {
+    public static ArrayList<Location> parseData() throws IOException {
+        ArrayList<Location> list = new ArrayList<>();
         FileReader data = new FileReader("LocationData.csv");
         BufferedReader br = new BufferedReader(data);
         String line;
@@ -34,7 +36,8 @@ public class LocationManager {
             String[] array = line.split(",");
             float latitude = Float.valueOf(array[2]);
             float longitude = Float.valueOf(array[3]);
-            addLocation(array[1], latitude, longitude, array[4], array[5], array[6], array[7], array[8], array[9], array[10]);
+            list.add(new Location(array[1], latitude, longitude, array[4], array[5], array[6], array[7], array[8], array[9], array[10]));
         }
+        return list;
     }
 }
