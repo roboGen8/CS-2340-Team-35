4c2202f21b8b5cb131e9cc62c4d8b4c6d471a29d
Brian Chea <briantc1997@gmail.com>
2018-10-09 15:47:48 -0400

    whack 2
    oh man

--- app/src/main/java/com/example/ben/cs2340/model/LocationAdapter.java
+++ app/src/main/java/com/example/ben/cs2340/model/LocationAdapter.java
@@ -10,7 +10,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
+//import com.bumptech.glide.Glide;
 import com.example.ben.cs2340.R;
 
 import java.util.ArrayList;
@@ -18,28 +18,28 @@ import java.util.List;
 
 public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.ViewHolder> {
 
-    private ArrayList<String> mNames = new ArrayList<>();
-    private ArrayList<String> mImages = new ArrayList<>();
-    private Context mContext;
+//    private ArrayList<String> mNames = new ArrayList<>();
+//    private ArrayList<String> mImages = new ArrayList<>();
+//    private Context mContext;
 
-    public LocationAdapter(Context context, ArrayList<String> locationNames, ArrayList<String> images) {
-        mContext = context;
-        mImages = images;
-        mContext = context;
-    }
+//    public LocationAdapter(Context context, ArrayList<String> locationNames, ArrayList<String> images) {
+//        mContext = context;
+//        mImages = images;
+//        mContext = context;
+//    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView nameTextView;
-//        public Button detailsButton;
-        public RelativeLayout parentLayout;
+        public Button detailsButton;
+//        public RelativeLayout parentLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
             nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
-//            detailsButton = (Button) itemView.findViewById(R.id.message_button);
-            parentLayout = (RelativeLayout) itemView.findViewById(R.id.parent_layout);
+            detailsButton = (Button) itemView.findViewById(R.id.message_button);
+//            parentLayout = (RelativeLayout) itemView.findViewById(R.id.parent_layout);
 
         }
     }
@@ -71,21 +71,21 @@ public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.ViewHo
         // Set item views based on your views and data model
         TextView textView = viewHolder.nameTextView;
         textView.setText(location.getName());
-//        Button button = viewHolder.detailsButton;
-//        button.setText("Details");
+        Button button = viewHolder.detailsButton;
+        button.setText("Details");
 
 //        Glide.with(mContext)
 //                .asBitmap()
 //                .load(mImages.get(position))
 //                .into(viewHolder.image);
 
-        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public  void onClick(View contactView) {
-//                Toast.makeText(mContext, location.get(position), Toast.LENGTH_SHORT).show();
-                Toast.makeText(mContext, "yeet", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public  void onClick(View contactView) {
+////                Toast.makeText(mContext, location.get(position), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(mContext, "yeet", Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 
     // Returns the total count of items in the list
